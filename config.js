895d4f0cdc6060393c82fd9d192ca7da3465a138
kerbril <jibril.jlassi25000@gmail.com>
2024-12-23 20:10:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -37,15 +37,13 @@ module.exports = {
    // Lavalink Server Details
 
   nodes: [
-    {
-        name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
-        reconnectTimeout: 5000,
-        reconnectTries: Infinity,
-        secure: false
-    },
+{
+  "identifier": "INZEWORLD.COM (DE)",
+  "password": "saher.inzeworld.com",
+  "host": "lava.inzeworld.com",
+  "port": 3128,
+  "secure": false
+},
  ]
 }
 /*
